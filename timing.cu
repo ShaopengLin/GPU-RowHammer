6c4273da6108606a7b5186b107bc620e6aca03b6
sp <ShaopengLin>
2024-01-31 22:57:06 -0500

    Refactoring

--- timing.cu
+++ timing.cu
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <assert.h>
+#include <iostream>
 #include <unistd.h>
 const uint64_t LAYOUT_SIZE = 16106127360; // 1073741824; //16106127360;
 
-__global__ void mapping_kernel(uint64_t *target, uint64_t *it_addr, long long *time)
-{
+__global__ void mapping_kernel(uint64_t *target, uint64_t *it_addr,
+                               long long *time) {
   volatile long long clock_start;
   volatile long long clock_end;
   volatile uint64_t temp;
@@ -14,58 +14,78 @@ __global__ void mapping_kernel(uint64_t *target, uint64_t *it_addr, long long *t
                "ld.u64.global.volatile %0, [%1];\n\t"
                "fence.sc.cta;\n\t"
                "discard.global.L2 [%1], 128;\n\t"
-               "}" : "=l"(temp) : "l"(it_addr));
+               "}"
+               : "=l"(temp)
+               : "l"(it_addr));
 
   /* Uncache target from previous accesses */
   asm volatile("{\n\t"
                "discard.global.L2 [%0], 128;\n\t"
-               "}" : "+l"(target));
+               "}"
+               : "+l"(target));
 
   /* Start Clock (Note: putting discard together with this will cause it
      to be optimized away) */
   asm volatile("{\n\t"
                "fence.sc.cta;\n\t"
                "mov.u64 %0, %%clock64;\n\t"
-               "}" : "=l"(clock_start));
+               "}"
+               : "=l"(clock_start));
 
   /* Test access to target. If it_addr is not a conflict should be fast */
   asm volatile("{\n\t"
                "ld.u64.global.volatile %0, [%1];\n\t"
-               "}" : "=l"(temp) : "l"(target));
+               "}"
+               : "=l"(temp)
+               : "l"(target));
 
   /* End clock */
   asm volatile("{\n\t"
                "mov.u64 %0, %%clock64;\n\t"
                "fence.sc.cta;\n\t"
-               "}" : "=l"(clock_end));
+               "}"
+               : "=l"(clock_end));
 
   *time = clock_end - clock_start;
 }
 
-int main(void)
-{
+long double test_single_pair(uint64_t *target, uint64_t *it_addr,
+                             uint32_t test_it, long long *time) {
+  long double avg_time = 0;
+  for (int i = 0; i < test_it; i++) {
+    mapping_kernel<<<1, 1>>>(target, it_addr, time);
+    cudaDeviceSynchronize();
+    avg_time += *time;
+  }
+  avg_time /= 10;
+  avg_time = (avg_time / 1620000000.0f) * 1000000000.0;
+  std::cout << it_addr << '\t' << avg_time << '\n';
+  return avg_time;
+}
+
+int main(void) {
   uint64_t *d_x;
   cudaMalloc(&d_x, LAYOUT_SIZE);
 
   long long *time;
   cudaHostAlloc(&time, sizeof(long long), cudaHostAllocDefault);
 
-  for (int i = 0; i < 1024 * 1024 * 1024; i++)
-  {
-    long long t = 0;
-    for (int j = 0; j < 10; j++)
-    {
-      mapping_kernel<<<1, 1>>>(d_x, d_x + i, time);
-      cudaDeviceSynchronize();
-      t += *time;
+  uint64_t *new_target = NULL;
+  for (int i = 0; i < 1024 * 10; i++) {
+    if (test_single_pair(d_x, d_x + i, 10, time) > 400 && i > 0) {
+      new_target = d_x + i;
+      break;
     }
-    t /= 10;
-    std::cout << d_x + i << '\t' <<  (t/1620000000.0f)*1000000000.0 << '\n';
   }
 
-  // size_t f, t;
-  // cudaMemGetInfo(&f, &t);
-  // std::cout << t - f << '\n';
-  // std::cout << d_x << '\n';
+  std::cout << "\nFound Conflict:" << '\n';
+  std::cout << test_single_pair(new_target, d_x, 10, time) << '\t'
+            << test_single_pair(d_x, new_target, 10, time) << '\n';
+  std::cout << "\nNew Target Normal Time:" << '\n';
+  std::cout << test_single_pair(new_target, new_target, 10, time) << '\n';
+
+  std::cout << "\nd_x Normal Time:" << '\n';
+  std::cout << test_single_pair(new_target, new_target, 10, time) << '\n';
+
   cudaFree(d_x);
 }
