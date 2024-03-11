64e3180ca82417d090f2a46522929757ae95d416
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2024-03-11 02:06:56 -0400

    Removes outdated files

--- timing_util.h
+++
@@ -1,5 +0,0 @@
-#ifndef GPU_ROWHAMMER_TIMING_UTIL_H
-#define GPU_ROWHAMMER_TIMING_UTIL_H
-long long toNS(long long time) { return (time / 1545000000.0f) * 1000000000.0; }
-
-#endif /* GPU_ROWHAMMER_TIMING_UTIL_H */
