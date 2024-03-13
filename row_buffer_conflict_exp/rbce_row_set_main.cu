a56762e7f8d42f5ed5321ad715244253e4c2939f
ShaopengLin <shaopeng.lin@mail.utoronto.ca>
2024-03-13 04:25:29 -0400

    Added row set to python script

---
+++ row_buffer_conflict_exp/rbce_row_set_main.cu
@@ -0,0 +1,68 @@
+#include "rbce_n_conflict_exp.cuh"
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+  /* Argument EXP_RANGE, EXP_IT, STEP */
+  rbce::N_Conflict nc_test(2, 0, std::stoull(argv[1]), 32);
+
+  /* Argument Threshold */
+  uint64_t threshold = std::stoull(argv[2]);
+
+  /* Offset to an address in Target Bank */
+  uint64_t offset_to_bank = std::stoull(argv[3]);
+
+  const uint8_t **addr = new const uint8_t *[2];
+  const uint8_t *addr_start = nc_test.get_addr_layout();
+  std::vector<const uint8_t *> conf_vec{addr_start + offset_to_bank};
+
+  /* Initialize address pairs */
+  addr[0] = addr_start;
+  addr[1] = addr_start + 1;
+  nc_test.repeat_n_addr_exp(addr, NULL);
+
+  std::string buf;
+  std::ifstream conf_set_file(argv[4]);
+
+  while (conf_set_file.peek() != EOF)
+  {
+    while (std::getline(conf_set_file, buf))
+    {
+      uint64_t conf_step = std::stoull(buf.c_str());
+
+      /* Address to be tested for whether it should be in conf_set */
+      addr[0] = addr_start + conf_step * 8;
+
+      bool all_conflict = true;
+      for (auto it = conf_vec.rbegin();
+           it != conf_vec.rend() && all_conflict && it != conf_vec.rbegin() +
+           5; it++)
+      {
+        /* Addresses already in conf_set */
+        addr[1] = *it;
+        uint64_t time = nc_test.repeat_n_addr_exp(addr, NULL);
+        all_conflict &= (time > threshold);
+      }
+
+      if (all_conflict)
+      {
+        conf_vec.push_back(addr[0]);
+        break;
+      }
+    }
+  }
+
+  std::ofstream row_set_file(argv[5]);
+  const uint8_t *initial = addr_start + offset_to_bank;
+  
+  row_set_file << (void *)(initial) << '\t' << offset_to_bank << '\n';
+  for (auto it = conf_vec.begin() + 1; it != conf_vec.end(); it++)
+  {
+    row_set_file << (const void *)(*it) << '\t' << *it - initial << '\n';
+    initial = *it;
+  }
+  delete[] addr;
+  conf_set_file.close();
+  return 0;
+}
--- run_timing_task.py
+++ run_timing_task.py
@@ -2,47 +2,84 @@ import argparse
 import subprocess
 
 
-def get_parser_gbo(parser):
-    parser_gbo = parser.add_parser(
-        "gbo",
-        help="Gets the bank address offsets in a bank",
+def get_parser_conf_set(parser):
+    parser_conf_set = parser.add_parser(
+        "conf_set",
+        help="Gets the conflict set of a bank given an address in a bank. The output file contains addresses offsets.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--range",
         type=int,
         help="The amount of bytes to iterate over.",
         default=8388608,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--it",
         type=int,
         help="Number of iterations when confirming conflict timing",
         default=10,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--step",
         type=int,
         help="How many step bytes to step over for each iteration.",
         default=32,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--threshold",
         type=int,
         help="Time value to be considered a conflict.",
         default=640,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--trgtBankOfs",
         type=int,
         help="Byte offset for address of the target bank. The program will get the offsets in the same bank as this address.",
         default=0,
     )
-    parser_gbo.add_argument(
+    parser_conf_set.add_argument(
         "--file",
         type=str,
         help="File to store offset.",
-        default="SAME_BANK_ADDR.txt",
+        default="CONF_SET.txt",
+    )
+
+
+def get_parser_row_set(parser):
+    parser_row_set = parser.add_parser(
+        "row_set",
+        help="Finds the rows in the bank given conflict set. Outputs file with row addresses and offset between previous row address found",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser_row_set.add_argument(
+        "--it",
+        type=int,
+        help="Number of iterations when confirming conflict timing",
+        default=10,
+    )
+    parser_row_set.add_argument(
+        "--threshold",
+        type=int,
+        help="Time value to be considered a conflict.",
+        default=640,
+    )
+    parser_row_set.add_argument(
+        "--trgtBankOfs",
+        type=int,
+        help="Byte offset for address of the target bank. The program will get the offsets in the same bank as this address.",
+        default=0,
+    )
+    parser_row_set.add_argument(
+        "inputFile",
+        type=str,
+        help="File to store offset.",
+    )
+    parser_row_set.add_argument(
+        "--outputFile",
+        type=str,
+        help="File to store offset.",
+        default="ROW_SET.txt",
     )
 
 
@@ -80,20 +117,27 @@ def get_parser_gen_time(parser):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="GPU Timing Channel Main",
+        prog="run_timing_task",
         description="Facade Runner that executes different CUDA timing channel tasks",
     )
     subparsers = parser.add_subparsers(
         dest="task_name", help="use `<command> -h` to see respective arguments"
     )
-    get_parser_gbo(subparsers)
+    get_parser_conf_set(subparsers)
     get_parser_gen_time(subparsers)
+    get_parser_row_set(subparsers)
 
     args = parser.parse_args()
     match args.task_name:
-        case "gbo":
+        case "conf_set":
             p = subprocess.Popen(
-                f"./out/build/rbce_gbo {args.range} {args.it} {args.step} {args.threshold} {args.trgtBankOfs} {args.file}",
+                f"./out/build/rbce_conf_set {args.range} {args.it} {args.step} {args.threshold} {args.trgtBankOfs} {args.file}",
+                shell=True,
+            )
+            p.wait()
+        case "row_set":
+            p = subprocess.Popen(
+                f"./out/build/rbce_row_set {args.it} {args.threshold} {args.trgtBankOfs} {args.inputFile} {args.outputFile}",
                 shell=True,
             )
             p.wait()
